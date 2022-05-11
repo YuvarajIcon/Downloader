f294bdaa0a785068090e12cb9c73eea27351ca27
Yuvaraj <yuvaraj@ncompass.in>
2022-05-11 17:21:28 +0530

    Initial commit

---
+++ Example/Downloader/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Downloader
+//
+//  Created by yuvaraj@ncompass.in on 05/11/2022.
+//  Copyright (c) 2022 yuvaraj@ncompass.in. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
